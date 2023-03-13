3056322d6a1dd37e15a1c69a60d7738217608765
cookiesandwich11 <127770828+cookiesandwich11@users.noreply.github.com>
2023-03-13 17:05:31 +0000

    Add files via upload

---
+++ rickroll-chromeos.ino
@@ -0,0 +1,52 @@
+#include "DigiKeyboard.h"
+void setup() {
+  // this is rickroll code for chromeos
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, KEY_T);
+  DigiKeyboard.delay(155);
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, KEY_N);
+  DigiKeyboard.delay(150);
+  DigiKeyboard.sendKeyStroke(KEY_F4);
+  DigiKeyboard.delay(150);
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, KEY_L);
+  DigiKeyboard.delay(150);
+  DigiKeyboard.print("chrome://settings");
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, KEY_F);
+  DigiKeyboard.delay(150);
+  DigiKeyboard.print("pop");
+  DigiKeyboard.delay(50);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for (int i = 0; i < 5; i++) {
+    DigiKeyboard.sendKeyStroke(43);
+    DigiKeyboard.delay(150);
+  }
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for (int i = 0; i < 15; i++) {
+    DigiKeyboard.sendKeyStroke(43);
+    DigiKeyboard.delay(150);
+  }
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for (int i = 0; i < 4; i++) {
+    DigiKeyboard.sendKeyStroke(43);
+    DigiKeyboard.delay(150);
+  }
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.print("https://cookie11-rickroll.tiiny.site/");
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, KEY_W);
+  DigiKeyboard.delay(150);
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, KEY_T);
+  DigiKeyboard.delay(150);
+  DigiKeyboard.print("https://cookie11-rickroll.tiiny.site/");
+  DigiKeyboard.delay(150);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+}
+
+void loop() {}
